a7bf8854d69d6d53a0864ef1ea8471e8e1a9487a
Aliks175 <108829258+Aliks175@users.noreply.github.com>
2025-11-11 00:38:54 +0300

    FixVers
    Стабильная версия без багов и проблем

--- Assets/GrannyKnight/Script/Enemy/Fairys/MoveDollyCart.cs
+++ Assets/GrannyKnight/Script/Enemy/Fairys/MoveDollyCart.cs
@@ -6,7 +6,6 @@ public class MoveDollyCart : MonoBehaviour
     [SerializeField] private AnimationCurve m_AnimationCurve;
     [SerializeField] private CinemachineSplineCart _cinemachineSplineCart;
     [SerializeField, Range(0, 1)] private float _speed;
-
     private bool _isPlay;
     private float _position;
     private float _tempPosition;
