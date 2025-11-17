d9295bd23f8b55d96539155c01332db7c35d6bcd
Aliks175 <108829258+Aliks175@users.noreply.github.com>
2025-11-17 17:06:15 +0300

    Squashed commit of the following:
    commit f73c46f4c8ed726cf497d00ed0c34ac5b562de87
    Merge: 786813c a8cc83b
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Mon Nov 17 17:52:49 2025 +0400
    
        Merge branch 'main' into Gosha-branch
    
    commit 786813caa6c2726a9cd14c20a06afd7dfb4680fd
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Mon Nov 17 17:27:17 2025 +0400
    
        Add physics weapon
    
    commit 4867ceb9def428238e8ac8881290f2b2399461d5
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Fri Nov 14 19:01:23 2025 +0400
    
        Dust Balance
    
    commit 87600ad988f9317600bbe760b884fc7eaffd6a60
    Merge: 5b170c9 a7bf885
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Fri Nov 14 17:38:15 2025 +0400
    
        Merge branch 'main' into Gosha-branch
    
    commit 5b170c919fa4cf9c74a9db971d7e1d7c891c7ce9
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Fri Nov 14 17:37:54 2025 +0400
    
        Change dust
    
    commit 9b7420fbf425d89766a2c96e275c075462807eb0
    Merge: 42a0262 1da58cc
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Mon Nov 10 22:14:37 2025 +0400
    
        Merge branch 'main' into Gosha-branch
    
    commit 42a0262f0b3d750ce1dcf2b76d982b187890cec1
    Merge: a96ea97 43c3992
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Mon Nov 10 14:17:11 2025 +0400
    
        Merge branch 'main' into Gosha-branch
    
    commit a96ea97c61298f4cc99c7bec23f9d3687eec3d3f
    Merge: 9dca6ec 35e5fea
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Sun Nov 9 18:18:01 2025 +0400
    
        Merge branch 'main' into Gosha-branch
    
    commit 9dca6ec7252415ad4c21ec66cdcbe5678fb3bae6
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Sun Nov 9 16:44:34 2025 +0400
    
        Balance minigame1
    
    commit c77ce6ccde94b7e38b175d9dd32ef980805f8f84
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Sat Nov 8 22:27:04 2025 +0400
    
        Create miniGame dust
    
    commit 73dd6c986d7bdfb3b41517e0ba14548e2325e738
    Merge: 2692532 5dc262c
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Sat Nov 8 19:50:46 2025 +0400
    
        Merge branch 'main' into Gosha-branch
    
    commit 2692532dbc38d1abfe8353eccf5331b2864fb452
    Merge: 3d81294 eb34d6c
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Fri Nov 7 19:16:07 2025 +0400
    
        Merge branch 'main' into Gosha-branch
    
    commit 3d812949d4651b597451be1914872cd910b44ef8
    Author: Georgy Mutafyan <mutafyan73@gmail.com>
    Date:   Wed Nov 5 22:31:09 2025 +0400
    
        Add plugins

--- Assets/GrannyKnight/Script/RunTime/System/InventorySystem/inventory/TypeItem/Weapon.cs
+++ Assets/GrannyKnight/Script/RunTime/System/InventorySystem/inventory/TypeItem/Weapon.cs
@@ -1,13 +1,19 @@
 using System;
+using UnityEngine;
 
 public class Weapon : Item
 {
     private DataWeapon DataWeapon;
     public TypeWeapon TypeWeapon => DataWeapon.TypeWeapon;
+    public  TypeShootPhysics TypeShoot => DataWeapon.TypeShoot;
     public int Damage => DataWeapon.Damage;
     public float TimeWaitFire => DataWeapon.TimeWaitFire;
     public float Range => DataWeapon.Range;
     public bool IsAutoFire => DataWeapon.IsAutoFire;
+    public float MaxForce => DataWeapon.MaxForce;
+    public float ForceMultiplier => DataWeapon.ForceMultiplier;
+    public float KoefCharge => DataWeapon.KoefCharge;
+    public GameObject ArrowPrefab => DataWeapon.ArrowPrefab;
 
     public override void Initialization(BazeItem bazeItem, IPlayerDatable characterData)
     {
@@ -25,15 +31,28 @@ public enum TypeWeapon
     none,
     Pistol,
     Stick,
+    Sling,
     Grenade
+}
+public enum TypeShootPhysics
+{
+    Raycast,
+    Physics
 }
 
 [Serializable]
 public struct DataWeapon
 {
     public TypeWeapon TypeWeapon;
+    public TypeShootPhysics TypeShoot;
     public int Damage;
     public float TimeWaitFire;
+    [Header("For Raycast weapon")]
     public float Range;
     public bool IsAutoFire;
+    [Header("For Physics weapon")]
+    public float MaxForce;
+    public float ForceMultiplier;
+    public float KoefCharge;
+    public GameObject ArrowPrefab;
 }
---
+++ Assets/GrannyKnight/Script/RunTime/System/WeaponSystem/Weapon/WeaponEffect/PhysicsEffect.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class PhysicsEffect : MonoBehaviour
+{
+   public int IdWeapon => _idWeapon;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private int _idWeapon;
+    private ControlViewMark _controlViewMark;
+    private PhysicsWeapon _testWeapon;
+    private int _shootAnimationID;
+    private int _endShootAnimationID;
+    private int _isShootAnimationID;
+
+    private void OnDisable()
+    {
+        if (_testWeapon != null)
+        {
+           // _testWeapon.Shoot -= Fire;
+           // _testWeapon.ResetShot -= ControlFire;
+        }
+    }
+
+    public void Initialization(PhysicsWeapon testWeapon, ControlViewMark controlViewMark)
+    {
+        _testWeapon = testWeapon;
+        _controlViewMark = controlViewMark;
+        _shootAnimationID = Animator.StringToHash("Shoot");
+        _endShootAnimationID = Animator.StringToHash("EndShoot");
+        _isShootAnimationID = Animator.StringToHash("IsShoot");
+        //.OnFire += Fire;
+       //_testWeapon.OnEndFire += ControlFire;
+    }
+
+    private void Fire(TypeShoot typeShoot)
+    {
+        if (_animator != null)
+        {
+            _animator.SetBool(_isShootAnimationID, true);
+            _animator.SetTrigger(_shootAnimationID);
+        }
+
+        if (_audioSource != null)
+        {
+            _audioSource.Play();
+        }
+
+        if (_particleSystem != null)
+        {
+            _particleSystem.Play();
+        }
+        CreateMark(typeShoot.raycastHit);
+    }
+
+    private void CreateMark(RaycastHit Pos)
+    {
+        if (Pos.point != Vector3.zero)
+        {
+            Mark tempMark = _controlViewMark.GetMark();
+            tempMark.transform.position = Pos.point;
+            tempMark.transform.rotation = Quaternion.LookRotation(Pos.normal);
+            tempMark.SetMark();
+        }
+    }
+
+    private void ControlFire()
+    {
+        if (_animator != null)
+        {
+            _animator.SetBool(_isShootAnimationID, false);
+            _animator.SetTrigger(_endShootAnimationID);
+            if (_particleSystem != null)
+            {
+                _particleSystem.Stop();
+            }
+        }
+    }
+}
