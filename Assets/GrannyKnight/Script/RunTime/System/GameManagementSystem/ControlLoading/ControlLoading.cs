f7605a67408d411c2750a57f7e35d4001f550adc
Aliks175 <108829258+Aliks175@users.noreply.github.com>
2025-11-28 10:32:44 +0300

    Fix Vers
    Стабильная версия

--- Assets/GrannyKnight/Script/RunTime/System/GameManagementSystem/ControlLoading/ControlLoading.cs
+++ Assets/GrannyKnight/Script/RunTime/System/GameManagementSystem/ControlLoading/ControlLoading.cs
@@ -1,27 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class ControlLoading : MonoBehaviour
 {
-    //[SerializeField] private ListScene listScene;
-
     public void LoadGame()
     {
         SceneManager.LoadSceneAsync((int)ListScene.Game, LoadSceneMode.Single);
-    }
-
-    public void LoadTestGame()
-    {
-        SceneManager.LoadSceneAsync((int)ListScene.TestGameBild, LoadSceneMode.Single);
-    }
-
-    public void LoadTestGameGamePlay()
-    {
-        SceneManager.LoadSceneAsync((int)ListScene.GameGamePlay, LoadSceneMode.Single);
     }
 
     public void LoadMenu()
@@ -34,6 +18,4 @@ public enum ListScene
 {
     Menu = 0,
     Game = 1,
-    GameGamePlay = 2,
-    TestGameBild = 3,
 }
