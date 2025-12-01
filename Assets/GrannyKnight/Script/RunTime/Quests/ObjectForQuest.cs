d4ec68f298fbe1e9be900ee23c712bef8199b516
Georgy Mutafyan <mutafyan73@gmail.com>
2025-12-01 18:10:18 +0400

    Add script for quest object

---
+++ Assets/GrannyKnight/Script/RunTime/Quests/ObjectForQuest.cs
@@ -0,0 +1,32 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class ObjectForQuest : MonoBehaviour
+{
+    [Header("Moving")]
+    [SerializeField] private Vector3 _moving;
+    [SerializeField] private float _timeForLoop;
+    [SerializeField] private Ease _easeMoving;
+    [Header("Rotate")]
+    [SerializeField] private Vector3 _rotation;
+    [SerializeField] private float _rotationTimeForLoop;
+    [SerializeField] private Ease _easeRotate;
+    private Tween _moveTween;
+    private Tween _rotationTween;
+    void Awake()
+    {
+        StartMove();
+    }
+    public void StartMove()
+    {
+        _moveTween = transform.DOMove(transform.position + _moving, _timeForLoop).SetEase(_easeMoving).SetLoops(-1, LoopType.Yoyo);
+        _rotationTween = transform.DORotate(_rotation, _rotationTimeForLoop).SetEase(_easeRotate).SetLoops(-1, LoopType.Incremental);
+        _moveTween.Play();
+        _rotationTween.Play();
+    }
+    private void OnDestroy()
+    {
+        _moveTween?.Kill();
+        _rotationTween?.Kill();
+    }
+}
