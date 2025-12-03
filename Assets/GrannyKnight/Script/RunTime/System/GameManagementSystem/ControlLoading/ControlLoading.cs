886728393e5f88700a5f5b80b6055f5325654a3f
Georgy Mutafyan <mutafyan73@gmail.com>
2025-12-04 00:48:31 +0400

    Update font and main menu

--- Assets/GrannyKnight/Script/RunTime/System/GameManagementSystem/ControlLoading/ControlLoading.cs
+++ Assets/GrannyKnight/Script/RunTime/System/GameManagementSystem/ControlLoading/ControlLoading.cs
@@ -12,6 +12,10 @@ public class ControlLoading : MonoBehaviour
     {
         SceneManager.LoadSceneAsync((int)ListScene.Menu, LoadSceneMode.Single);
     }
+    public void LoadFreeGame()
+    {
+        SceneManager.LoadSceneAsync((int)ListScene.FreeGame, LoadSceneMode.Single);
+    }
 
     public void Exit()
     {
@@ -23,4 +27,5 @@ public enum ListScene
 {
     Menu = 0,
     Game = 1,
+    FreeGame = 2
 }
