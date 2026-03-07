aaa50a1314b5abacd16cb18655f51837d2da8ed1
Georgy Mutafyan <mutafyan73@gmail.com>
2026-03-07 21:16:18 +0400

    Change UI logic
    Swap to DoTween

--- Assets/GrannyKnight/Script/RunTime/System/UiSystem/UIButton.cs
+++ Assets/GrannyKnight/Script/RunTime/System/UiSystem/UIButton.cs
@@ -1,50 +1,79 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.EventSystems;
 
-public class UIButton : MonoBehaviour
+public class UIButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     [Header("Для анимации кнопки на наведении")]
-    [SerializeField] private float _scaleMultiplier = 1.1f;
-    [SerializeField] private float _animationDuration = 0.2f;
+    [SerializeField] private float _holdScale = 1.1f;
+    [SerializeField] private float _holdDuration = 0.2f;
+    [SerializeField] private Color _holdColor = Color.white;
     [Header("Для анимации кнопки на нажатии")]
     [SerializeField] private float _clickScale = 0.9f;
     [SerializeField] private float _clickDuration = 0.1f;
+    [SerializeField] private Color _clickColor = Color.gray;
     private Button _button;
+    private Image _image;
     private Vector3 _originalScale;
-    private Tween _tween;
+    private Color _originalColor;
+    private Tween _scaleTween;
+    private Tween _colorTween;
      void Start()
     {
         _button = GetComponent<Button>();
+        _image = GetComponent<Image>();
         _originalScale = transform.localScale;
+        if (_image != null) _originalColor = _image.color;
+    }
+    void OnEnable()
+    {
+        if (_originalScale !=  Vector3.zero) transform.localScale = _originalScale;
+        if (_image != null) _image.color = _originalColor;
     }
 
-    public void OnPointerEnter()
+    public void OnPointerEnter(PointerEventData eventData)
     {
         if (_button.interactable)
         {
-            _tween = transform.DOScale(_originalScale * _scaleMultiplier, _animationDuration);
-            _tween.Play();
+            _scaleTween = transform.DOScale(_originalScale * _holdScale, _holdDuration);
+            _scaleTween.Play();
+            if (_image != null)
+            {
+                _colorTween = _image.DOColor(_holdColor, _holdDuration);
+                _colorTween.Play();
+            }
         }
-        Debug.Log("workong");
     }
-
-    public void OnPointerExit()
+    public void OnPointerExit(PointerEventData eventData)
     {
-        _tween = transform.DOScale(_originalScale, _animationDuration);
-        _tween.Play();
+        _scaleTween = transform.DOScale(_originalScale, _holdDuration);
+        _scaleTween.Play();
+        if (_image != null)
+        {
+            _colorTween = _image.DOColor(_originalColor, _holdDuration);
+            _colorTween.Play();
+        }
     }
-    public void AnimateClick()
+    public void OnPointerDown(PointerEventData eventData)
     {
         if (_button.interactable)
         {
-            _tween = transform.DOScale(_originalScale * _clickScale, _clickDuration)
+            _scaleTween = transform.DOScale(_originalScale * _clickScale, _clickDuration)
                 .OnComplete(() => 
                 {
-                    _tween = transform.DOScale(_originalScale, _clickDuration);
+                    _scaleTween = transform.DOScale(_originalScale, _clickDuration);
                 });
-            _tween.Play();
+            _scaleTween.Play();
+            if (_image != null)
+            {
+                _colorTween = _image.DOColor(_clickColor, _clickDuration)
+                    .OnComplete(() =>
+                    {
+                        _colorTween = _image.DOColor(_originalColor, _clickDuration);
+                    });
+                _colorTween.Play();
+            }
         }
     }
-
 }
