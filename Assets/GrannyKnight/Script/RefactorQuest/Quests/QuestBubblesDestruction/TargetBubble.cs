21a1e25cee1c661c3e35fa657962230c2fb80999
Aliks175 <108829258+Aliks175@users.noreply.github.com>
2026-05-03 19:04:36 +0300

    Fix

--- Assets/GrannyKnight/Script/RefactorQuest/Quests/QuestBubblesDestruction/TargetBubble.cs
+++ Assets/GrannyKnight/Script/RefactorQuest/Quests/QuestBubblesDestruction/TargetBubble.cs
@@ -3,20 +3,33 @@ using Zenject;
 
 public class TargetBubble : MonoBehaviour, IHealtheble, ITarget
 {
-    private ControlTarget _controlTarget;
-
     public GameObject Body => gameObject;
+    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private GameObject _body;
+    private Collider _collider;
+    private ControlTarget _controlTarget;
+    private bool _isAlive;
 
     [Inject]
     public void Construct(ControlTarget controlBubbles)
     {
         _controlTarget = controlBubbles;
         _controlTarget.AddBubbles(this);
+        _isAlive = true;
+    }
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider>();
     }
 
     public void TakeDamage(float damage)
     {
+        if (!_isAlive) { return; }
+        _isAlive = false;
         _controlTarget.AddCountTargetDestruction();
-        gameObject.SetActive(false);
+        _collider.enabled = false;
+        _body.SetActive(false);
+        _particleSystem.Play();
     }
 }
