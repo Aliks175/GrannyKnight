e9848c2a6435630b2e6a09f41472cbd32b3db2c7
Aliks175 <108829258+Aliks175@users.noreply.github.com>
2026-05-04 12:01:14 +0300

    Fix

--- Assets/GrannyKnight/Script/RefactorQuest/Quests/QuestBubblesDestruction/Sub/SimpleMove.cs
+++ Assets/GrannyKnight/Script/RefactorQuest/Quests/QuestBubblesDestruction/Sub/SimpleMove.cs
@@ -7,6 +7,7 @@ public class SimpleMove : MonoBehaviour
     [Header("SimpleMove")]
     [SerializeField] private Vector3 _direction;
     [SerializeField] private float _duration;
+    private Tween _tween;
     private float _speed;
 
     private void Start()
@@ -14,17 +15,22 @@ public class SimpleMove : MonoBehaviour
         RundomRange().Forget();
     }
 
+    private void OnDisable()
+    {
+        _tween?.Kill();
+    }
+
     private void SimpleLoopMove()
     {
-        Tween tween = transform.DOMove(transform.position + _direction, _speed).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.OutSine);
-        tween.Play();
+        _tween = transform.DOMove(transform.position + _direction, _speed).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.OutSine);
+        _tween.Play();
     }
-    
+
     private async UniTask RundomRange()
     {
         _speed = Random.Range(0, 1f);
         await UniTask.Delay(System.TimeSpan.FromSeconds(_speed));
-        _speed =+ _duration;
+        _speed = +_duration;
         SimpleLoopMove();
     }
 }
