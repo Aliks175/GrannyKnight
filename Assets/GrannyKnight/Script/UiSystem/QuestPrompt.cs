265d001a7d591daaebf088660bd16bbb44c0fe2a
Aliks175 <108829258+Aliks175@users.noreply.github.com>
2026-05-19 22:17:58 +0300

    AddTestUiQestPanel

---
+++ Assets/GrannyKnight/Script/UiSystem/QuestPrompt.cs
@@ -0,0 +1,61 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class QuestPrompt : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] protected RectTransform _backImage;
+    [SerializeField] protected RectTransform _bodyText;
+    [SerializeField] protected float _endPosition;
+    private Vector2 _bodyTextStartPos;
+    private Vector2 _bodyTextEndPos;
+    private Tween _open;
+    private Tween _close;
+    private Tween _shake;
+    private bool _isVisible;
+    // 30 340
+
+    private void Start()
+    {
+        _bodyTextStartPos = _bodyText.anchoredPosition;
+        _bodyTextEndPos = _bodyTextStartPos;
+        _bodyTextEndPos.y = _endPosition;
+        _isVisible = false;
+
+        _open = _bodyText.DOAnchorPos(_bodyTextStartPos, 1.2f)
+                .SetEase(Ease.OutBack)
+                .From(_bodyTextEndPos)
+                .SetAutoKill(false);
+
+        _close = _bodyText.DOAnchorPos(_bodyTextEndPos, 1f)
+               .SetEase(Ease.InSine)
+               .From(_bodyTextStartPos)
+               .SetAutoKill(false);
+
+        _shake = _backImage.DOShakeAnchorPos(0.5f, 20, 10)
+                .SetAutoKill(false);
+
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.tabKey.wasPressedThisFrame)
+        {
+            Debug.Log($"Press  _isVisible {_isVisible}");
+            if (_isVisible)
+            {
+                _open.Restart();
+            }
+            else
+            {
+                _close.Restart();
+            }
+            if (!_shake.IsPlaying())
+            {
+                _shake.Restart();
+            }
+            _isVisible = !_isVisible;
+        }
+    }
+}
